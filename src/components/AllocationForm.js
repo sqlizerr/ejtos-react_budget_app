fba0ab18557b3af419775406797e130272d10965
Vedang <vedangresserve@gmail.com>
2023-12-18 13:15:33 -0500

    final changes

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -50,14 +50,14 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-                    <label for='cost' value={currency}></label>
+                    <span style={{marginLeft: '2rem'}}>{currency}</span>
                     <input
                         required='required'
                         type='number'
                         label={currency}
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '1rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
